1fc1329c8bd53d9b32e787b425d3879d9b44b381
Елизавета Асеева <aseyshaaa@mail.ru>
2023-12-07 12:49:35 +0700

    sama proga

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 def count_letters(word):
     return len(word)
+
+word = "Hello, World!"
+letter_count = count_letters(word)
+
+# Запись результата в журнал
+journal = {'task': 'Подсчет количества букв в слове', 'word': word, 'result': letter_count}
+print(journal)
